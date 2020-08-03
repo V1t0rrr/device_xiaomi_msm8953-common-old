1403c62a1a842c788be77aae8130d175601b70d6
vitor00almei <pondebatata@outlook.com>
2020-08-03 03:25:50 +0000

    msm8953-common: drop seccomp_policy
    * already exists on source

--- msm8953.mk
+++ msm8953.mk
@@ -302,10 +302,6 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-# Seccomp policy
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
